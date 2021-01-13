4ccc673eb496a8e63d7d53a930218d5abfa2ad50
rygo6-MS <rygoodri@microsoft.com>
2021-01-12 20:05:15 -0800

    Fix bugs in ava crypt validation.

--- Scripts/AvaCryptController.cs
+++ Scripts/AvaCryptController.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using AnimatorController = UnityEditor.Animations.AnimatorController;
 using AnimatorControllerLayer = UnityEditor.Animations.AnimatorControllerLayer;
 using BlendTree = UnityEditor.Animations.BlendTree;
+using Object = UnityEngine.Object;
 
 namespace GeoTetra.GTAvaCrypt
 {
@@ -15,6 +16,9 @@ namespace GeoTetra.GTAvaCrypt
         private readonly string[] AvaCryptKeyNames = {"AvaCryptKey0", "AvaCryptKey1", "AvaCryptKey2", "AvaCryptKey3"};
         private readonly AnimationClip[] _clips0 = new AnimationClip[4];
         private readonly AnimationClip[] _clips100 = new AnimationClip[4];
+
+        private const string StateMachineName = "AvaCryptKey{0} State Machine";
+        private const string BlendTreeName = "AvaCryptKey{0} Blend Tree";
         
         public void ValidateAnimations(GameObject gameObject, AnimatorController controller)
         {
@@ -44,6 +48,8 @@ namespace GeoTetra.GTAvaCrypt
                     _clips100[i].SetCurve(transformPath, typeof(SkinnedMeshRenderer), $"material._Key{i}", new AnimationCurve(new Keyframe(0, 100)));
                 }
             }
+            
+            AssetDatabase.SaveAssets();
         }
 
         private void ValidateClip(GameObject gameObject, AnimatorController controller, int index)
@@ -99,6 +105,7 @@ namespace GeoTetra.GTAvaCrypt
                 if (controller.parameters.All(parameter => parameter.name != keyName))
                 {
                     controller.AddParameter(keyName, AnimatorControllerParameterType.Float);
+                    AssetDatabase.SaveAssets();
                     Debug.Log($"Adding parameter: {keyName}");
                 }
                 else
@@ -110,43 +117,106 @@ namespace GeoTetra.GTAvaCrypt
 
         public void ValidateLayers(AnimatorController controller)
         {
-            // Easiest just to remove and re-add the AvaCrypt key layers.
-
-            foreach (string keyName in AvaCryptKeyNames)
-            {
-                for (int l = controller.layers.Length - 1; l > -1; --l)
-                {
-                    if (controller.layers[l].name == keyName) controller.RemoveLayer(l);
-                }
-            }
-
             for (int i = 0; i < AvaCryptKeyNames.Length; ++i)
             {
-                AnimatorControllerLayer layer0 = CreateLayer(i);
-                controller.AddLayer(layer0);
-                Debug.Log($"Adding layer: {AvaCryptKeyNames[i]}");
+                if (controller.layers.All(l => l.name != AvaCryptKeyNames[i]))
+                {
+                    CreateLayer(i, controller);
+                }
+                else
+                {
+                    Debug.Log($"Layer already existing: {AvaCryptKeyNames[i]}");
+                    AnimatorControllerLayer layer = controller.layers.FirstOrDefault(l => l.name == AvaCryptKeyNames[i]);
+
+                    if (layer.stateMachine == null)
+                    {
+                        Debug.Log("Layer missing state machine.");
+                        
+                        // Try to delete blend tree and layers if by chance they still exist for some reason.
+                        DeleteObjectFromController(controller, string.Format(StateMachineName, i));
+                        DeleteObjectFromController(controller, string.Format(BlendTreeName, i));
+                        
+                        controller.RemoveLayer(controller.layers.ToList().IndexOf(layer));
+                        
+                        CreateLayer(i, controller);
+                    }
+                    else
+                    {
+                        ValidateLayerBlendTree(i, layer, controller);
+                    }
+                }
+            }
+        }
+        
+        private void ValidateLayerBlendTree(int index, AnimatorControllerLayer layer, AnimatorController controller)
+        {
+            string blendTreeName = string.Format(BlendTreeName, index);
+            
+            if (layer.stateMachine.states.All(s => s.state.name != blendTreeName))
+            {
+                Debug.Log($"Layer missing blend tree. {blendTreeName}");
+                DeleteObjectFromController(controller, blendTreeName);
+                AddBlendTree(index, layer, controller);
+            }
+            else
+            {
+                Debug.Log($"Layer Blend Tree Validated {blendTreeName}.");
+                
+                BlendTree blendTree = layer.stateMachine.states.FirstOrDefault(s => s.state.name == blendTreeName).state.motion as BlendTree;
+                
+                // Just re-assign since ChildMotions aren't their own ScriptableObjects.
+                ChildMotion childMotion0 = new ChildMotion
+                {
+                    motion = _clips0[index],
+                    timeScale = 1
+                };
+                ChildMotion childMotion1 = new ChildMotion
+                {
+                    motion = _clips100[index],
+                    timeScale = 1
+                };
+                blendTree.children = new ChildMotion[2] {childMotion0, childMotion1};
+                
+                AssetDatabase.SaveAssets();;
             }
         }
 
-        private AnimatorControllerLayer CreateLayer(int index)
+        private void CreateLayer(int index, AnimatorController controller)
         {
+            Debug.Log($"Creating layer: {AvaCryptKeyNames[index]}");
+            
+            string controllerPath = AssetDatabase.GetAssetPath(controller);
+            
             AnimatorControllerLayer layer = new AnimatorControllerLayer
             {
                 name = AvaCryptKeyNames[index],
                 defaultWeight = 1,
                 stateMachine = new AnimatorStateMachine(),
             };
+            layer.stateMachine.name = string.Format(StateMachineName, index);
+            
+            controller.AddLayer(layer);
+            AssetDatabase.AddObjectToAsset(layer.stateMachine, controllerPath);
+            AssetDatabase.SaveAssets();
+            
+            AddBlendTree(index, layer, controller);
+        }
 
-            AnimatorState state = layer.stateMachine.AddState("Blend Tree");
+        private void AddBlendTree(int index, AnimatorControllerLayer layer, AnimatorController controller)
+        {
+            string controllerPath = AssetDatabase.GetAssetPath(controller);
+            string blendTreeName = string.Format(BlendTreeName, index);
+            
+            AnimatorState state = layer.stateMachine.AddState(blendTreeName);
             state.speed = 1;
-
+            
             BlendTree blendTree = new BlendTree
             {
-                name = "Blend Tree",
+                name = blendTreeName,
                 blendType = BlendTreeType.Simple1D,
                 blendParameter = AvaCryptKeyNames[index],
             };
-
+            
             ChildMotion childMotion0 = new ChildMotion
             {
                 motion = _clips0[index],
@@ -158,10 +228,24 @@ namespace GeoTetra.GTAvaCrypt
                 timeScale = 1
             };
             blendTree.children = new ChildMotion[2] {childMotion0, childMotion1};
-
-            state.motion = blendTree;
             
-            return layer;
+            state.motion = blendTree;
+            AssetDatabase.AddObjectToAsset(blendTree, controllerPath);
+            
+            AssetDatabase.SaveAssets();
+        }
+
+        private void DeleteObjectFromController(AnimatorController controller, string name)
+        {
+            string controllerPath = AssetDatabase.GetAssetPath(controller);
+            foreach (Object subObject in AssetDatabase.LoadAllAssetsAtPath(controllerPath))
+            {
+                if (subObject.hideFlags == HideFlags.None && subObject.name == name)
+                {
+                    AssetDatabase.RemoveObjectFromAsset(subObject);
+                }
+            }
+            AssetDatabase.SaveAssets();
         }
     }
 }
