23e914957cdf7baca59725be9d0a4cdf6308680b
Karet <5xa1xa@gmail.com>
2022-01-18 22:25:06 -0600

    Added Protected Avatar Pedistal

---
+++ Protected Avatar Pedistal/ProtectedAvatarPedestal.cs
@@ -0,0 +1,28 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ProtectedAvatarPedestal : UdonSharpBehaviour
+{
+    [SerializeField] GameObject Mirror;
+    VRC_AvatarPedestal avatarPedestal;
+    private void Start()
+    {
+        avatarPedestal = (VRC_AvatarPedestal)GetComponent(typeof(VRC_AvatarPedestal));
+    }
+    private void Interact()
+    {
+        avatarPedestal.SetAvatarUse(Networking.LocalPlayer);
+        Networking.LocalPlayer.UseAttachedStation();
+    }
+    public override void OnStationEntered()
+    {
+        Mirror.SetActive(true);
+    }
+    public override void OnStationExited()
+    {
+        Mirror.SetActive(false);
+    }
+}
