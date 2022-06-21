7a12477563c0cf8a5534c0b4e5e745578d0ef71b
Karet <5xa1xa@gmail.com>
2022-06-21 07:07:32 -0500

    Improved? the custom inspector

--- Editor/AvaCryptRootEditor.cs
+++ Editor/AvaCryptRootEditor.cs
@@ -14,73 +14,158 @@ namespace GeoTetra.GTAvaCrypt
         SerializedProperty _vrcSavedParamsPathProperty;
         bool _debugFoldout = false;
 
+        Texture2D HeaderTexture;
         void OnEnable()
         {
             _distortRatioProperty = serializedObject.FindProperty("_distortRatio");
             _keysProperty = serializedObject.FindProperty("_bitKeys");
             _vrcSavedParamsPathProperty = serializedObject.FindProperty("_vrcSavedParamsPath");
+
+            HeaderTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Packages/com.geotetra.gtacrypt/Textures/Titlebar.png", typeof(Texture2D));
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
 
+            if (GUILayout.Button(new GUIContent(HeaderTexture, "Vist my Discord for help!"), EditorStyles.label, GUILayout.Height(Screen.width / 8)))
+            {
+                Application.OpenURL("https://discord.gg/nbzqtaVP9J");
+            }
+            
             AvaCryptV2Root avaCryptV2Root = target as AvaCryptV2Root;
             
+            //Do the big important buttons
             EditorGUILayout.Space();
-            GUILayout.Label("Validate the AnimatorController, then create encrypted avatar.");
-            if (GUILayout.Button("Encrypt Avatar"))
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button(new GUIContent("Encrypt Avatar", "Validate the AnimatorController, then create encrypted avatar."), GUILayout.Height(Screen.width / 10), GUILayout.Width((Screen.width / 2) - 20f)))
             {
                 avaCryptV2Root.EncryptAvatar();
             }
-            
-            EditorGUILayout.Space();
-            GUILayout.Label("Write your keys to saved attributes!");
-            if (GUILayout.Button("Write Keys"))
+
+            if (GUILayout.Button(new GUIContent("Write Keys", "Write your keys to saved attributes!"), GUILayout.Height(Screen.width / 10), GUILayout.Width((Screen.width / 2) - 20f)))
             {
                 avaCryptV2Root.WriteBitKeysToExpressions();
             }
-            EditorGUILayout.PropertyField(_vrcSavedParamsPathProperty);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
 
-            EditorGUILayout.Space();
-            EditorGUILayout.PropertyField(_distortRatioProperty);
-            EditorGUILayout.Space();
-            
+            //Do the properties
+            GUILayout.BeginHorizontal();
+            GUILayout.BeginVertical(GUILayout.Width((Screen.width / 2) - 20f));
+            _distortRatioProperty.floatValue = GUILayout.HorizontalSlider(_distortRatioProperty.floatValue, .1f, .4f);
+            GUILayout.Space(15);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Distort Ratio:");
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.FloatField(_distortRatioProperty.floatValue);
+            GUILayout.EndHorizontal();
+            GUILayout.Label("Set high enough so your encrypted mesh is visuall. Default = .1", EditorStyles.wordWrappedLabel);
+            GUILayout.EndVertical();
+
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginVertical(GUILayout.Width((Screen.width / 2) - 20f));
+            GUILayout.Space(3);
+            GUILayout.Label("VRC Saved Paramters Path");
+            _vrcSavedParamsPathProperty.stringValue = EditorGUILayout.TextField(_vrcSavedParamsPathProperty.stringValue);
+            GUILayout.Label("Ensure this is pointing to your LocalAvatarData folder!", EditorStyles.wordWrappedLabel);
+            GUILayout.EndVertical();
+            GUILayout.EndHorizontal();
+
             EditorGUILayout.Separator();
 
-            EditorGUILayout.Space();
-            for (int i = 0; i < _keysProperty.arraySize; ++i)
+            using (new GUILayout.HorizontalScope(EditorStyles.helpBox))
             {
-                EditorGUILayout.PropertyField(_keysProperty.GetArrayElementAtIndex(i), new GUIContent($"BitKey{i}"));
-            }
+                GUILayout.BeginVertical();
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("Bitkeys", EditorStyles.boldLabel);
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
 
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("These are the keys used to encrypt the mesh.");
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                GUILayout.Space(5);
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.BeginVertical();
+
+                //Display in 4 columns
+                for (int i = 0; i < _keysProperty.arraySize/4; i++)
+                {
+                    GUILayout.BeginHorizontal();
+                    _keysProperty.GetArrayElementAtIndex(i).boolValue = GUILayout.Toggle(_keysProperty.GetArrayElementAtIndex(i).boolValue, "BitKey" + i);
+                    GUILayout.EndHorizontal();
+                    GUILayout.Space(5f);
+                }
+                GUILayout.EndVertical();
+                GUILayout.FlexibleSpace();
+                GUILayout.BeginVertical();
+                for (int i = _keysProperty.arraySize / 4; i < _keysProperty.arraySize / 2; i++)
+                {
+                    GUILayout.BeginHorizontal();
+                    _keysProperty.GetArrayElementAtIndex(i).boolValue = GUILayout.Toggle(_keysProperty.GetArrayElementAtIndex(i).boolValue, "BitKey" + i);
+                    GUILayout.EndHorizontal();
+                    GUILayout.Space(5f);
+                }
+                GUILayout.EndVertical();
+                GUILayout.FlexibleSpace();
+                GUILayout.BeginVertical();
+                for (int i = _keysProperty.arraySize / 2; i < (_keysProperty.arraySize / 4) * 3 ; i++)
+                {
+                    GUILayout.BeginHorizontal();
+                    _keysProperty.GetArrayElementAtIndex(i).boolValue = GUILayout.Toggle(_keysProperty.GetArrayElementAtIndex(i).boolValue, "BitKey" + i);
+                    GUILayout.EndHorizontal();
+                    GUILayout.Space(5f);
+                }
+                GUILayout.EndVertical();
+                GUILayout.FlexibleSpace();
+                GUILayout.BeginVertical();
+                for (int i = (_keysProperty.arraySize / 4) * 3; i < _keysProperty.arraySize; i++)
+                {
+                    GUILayout.BeginHorizontal();
+                    _keysProperty.GetArrayElementAtIndex(i).boolValue = GUILayout.Toggle(_keysProperty.GetArrayElementAtIndex(i).boolValue, "BitKey" + i);
+                    GUILayout.EndHorizontal();
+                    GUILayout.Space(5f);
+                }
+                GUILayout.EndVertical();
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+
+                //Generate key button
+                EditorGUILayout.Space();
+                if (GUILayout.Button(new GUIContent("Generate new Keys", "Generate new key overriding old one. Will need to write key again!")))
+                {
+                    avaCryptV2Root.GenerateNewKey();
+                }
+            }
+            
+            GUILayout.EndHorizontal();
             EditorGUILayout.Space();
             EditorGUILayout.Separator();
             _debugFoldout = EditorGUILayout.Foldout(_debugFoldout, "Debug");
             if (_debugFoldout)
             {
                 EditorGUILayout.Space();
-                GUILayout.Label("Validate all parameters, layers and animations are correct in this avatar's AnimatorController.");
-                if (GUILayout.Button("Validate Animator Controller"))
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button(new GUIContent("Validate Animator Controller", "Validate all parameters, layers and animations are correct in this avatar's AnimatorController."), GUILayout.Height(Screen.width / 10), GUILayout.Width((Screen.width / 2) - 20f)))
                 {
                     avaCryptV2Root.ValidateAnimatorController();
                 }
-                
-                EditorGUILayout.Space();
-                GUILayout.Label("Deletes all the objects AvaCrypt V1 wrote to your controller.");
-                if (GUILayout.Button("Delete AvaCryptV1 Objects From Controller"))
+
+                if (GUILayout.Button(new GUIContent("Delete AvaCryptV1 Objects From Controller", "Deletes all the objects AvaCrypt V1 wrote to your controller."), GUILayout.Height(Screen.width / 10), GUILayout.Width((Screen.width / 2) - 20f)))
                 {
                     avaCryptV2Root.DeleteAvaCryptV1ObjectsFromController();
                 }
-                
-                EditorGUILayout.Space();
-                GUILayout.Label("Generate new key overriding old one. Will need to write key again!");
-                if (GUILayout.Button("Generate Key"))
-                {
-                    avaCryptV2Root.GenerateNewKey();
-                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
             }
-
             serializedObject.ApplyModifiedProperties();
         }
     }
